6959ceee548d8f5cda29be6db2cda4a0c1871814
Edward Kim <itgma1231@gmail.com>
2023-08-07 15:11:18 -0400

    index.js inquirer prompts completed

---
+++ index.js
@@ -0,0 +1,60 @@
+const inquirer = require('inquirer')
+const validateColor = require('validate-color').default
+
+const promptInquirer = () => {
+    console.log('Generate an SVG logo!')
+
+    inquirer.prompt([
+        {
+            name: 'text',
+            type: 'input',
+            message: 'Enter a text you would like to display in the logo. (Up to three characters max):',
+            validate: (letters) => {
+                if (letters && letters.length <= 3) {
+                    return true
+                } else {
+                    console.log('\nPlease enter a text for your logo (Up to three characters max)')
+                    return false
+                }
+            }
+        },
+        {
+            name: 'textColor',
+            type: 'input',
+            message: 'What color would you like the text to be? (Enter a valid HTML color, hex, rgb, hsl, hwb, etc.):',
+            validate: (textColor) => {
+                const colorCheck = validateColor(textColor)
+
+                if (textColor && colorCheck) {
+                    return true                    
+                } else {
+                    console.log('\nPlease enter a valid HTML color')
+                    return false
+                }
+            }
+        },
+        {
+            name: 'shape',
+            type: 'list',
+            message: 'Shape of the logo? Choose one of the following:',
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            name: 'shapeColor',
+            type: 'input',
+            message: 'What would you like the color of the shape to be? (Enter a valid HTML color, hex, rgb, hsl, hwb, etc.):',
+            validate: (shapeColor) => {
+                const colorCheck = validateColor(shapeColor)
+
+                if (shapeColor && colorCheck) {
+                    return true
+                } else {
+                    console.log('\nPlease a valid HTML color')
+                    return false
+                }
+            }
+        }
+    ]).then(data => console.log(data))
+}
+
+promptInquirer()
