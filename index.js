87cc42d1366e8c53294af52fe85d7604c0585ce1
Edward Kim <itgma1231@gmail.com>
2023-08-09 16:25:03 -0400

    added comments

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// CommonJS import statements; validate-color package installed for HTML color validation
 const inquirer = require('inquirer')
 const { writeFile } = require('fs').promises
 const generateSVG = require('./lib/generateSVG')
 const validateColor = require('validate-color').default
 
+// Function to start inquirer.js in terminal
 const promptInquirer = async () => {
     console.log('Generate an SVG logo!')
 
@@ -13,6 +15,7 @@ const promptInquirer = async () => {
                 type: 'input',
                 message: 'Enter a text you would like to display in the logo. (Up to three characters max):',
                 validate: (letters) => {
+                    // Checks to see if text inputed is within 3 characters
                     if (letters && letters.length <= 3) {
                         return true
                     } else {
@@ -26,6 +29,7 @@ const promptInquirer = async () => {
                 type: 'input',
                 message: 'What color would you like the text to be?',
                 validate: (textColor) => {
+                    // Checks color validation for text input using validate-color
                     const colorCheck = validateColor(textColor)
     
                     if (textColor && colorCheck) {
@@ -47,6 +51,7 @@ const promptInquirer = async () => {
                 type: 'input',
                 message: 'What color would you like the shape to be?',
                 validate: (shapeColor) => {
+                    // Checks color validation for shape color using validate-color
                     const colorCheck = validateColor(shapeColor)
     
                     if (shapeColor && colorCheck) {
@@ -60,6 +65,7 @@ const promptInquirer = async () => {
         ])
         const result = generateSVG(data)
 
+        // fs.promises.writeFile() to the ./examples folder
         await writeFile('./examples/logo.svg', result)
 
         console.log('SVG logo generated!')
